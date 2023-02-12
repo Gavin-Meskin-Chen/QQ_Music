f92638a6a74eda9d4f9d409050053344610f6c7e
Gavin Chen <47521241+Gavin-Meskin-Chen@users.noreply.github.com>
2023-02-12 18:00:28 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
+console.log("\n %c HeoMusic 开源静态音乐播放器 v1.1 %c https://github.com/zhheo/HeoMusic \n", "color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;")
+
 var heo = {
   // 音乐节目切换背景
   changeMusicBg: function (isChangeBg = true) {
     const heoMusicBg = document.getElementById("music_bg")
 
     if (isChangeBg) {
-      // player listswitch 会进入此处
+      // player loadeddata 会进入此处
       const musiccover = document.querySelector("#heoMusic-page .aplayer-pic");
-      heoMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
+      var img = new Image();
+      img.src = extractValue(musiccover.style.backgroundImage);
+      img.onload = function() {
+        heoMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
+      };
     } else {
       // 第一次进入，绑定事件，改背景
       let timer = setInterval(()=>{
         const musiccover = document.querySelector("#heoMusic-page .aplayer-pic");
         // 确保player加载完成
-        console.info(heoMusicBg);
+        // console.info(heoMusicBg);
         if (musiccover) {
           clearInterval(timer)
-          heoMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
           // 绑定事件
           heo.addEventListenerChangeMusicBg();
         }
@@ -24,14 +29,36 @@ var heo = {
   },
   addEventListenerChangeMusicBg: function () {
     const heoMusicPage = document.getElementById("heoMusic-page");
-    const aplayerIconMenu = heoMusicPage.querySelector(".aplayer-info .aplayer-time .aplayer-icon-menu");
-
     heoMusicPage.querySelector("meting-js").aplayer.on('loadeddata', function () {
       heo.changeMusicBg();
-      console.info('player loadeddata');
+      // console.info('player loadeddata');
     });
-  }
+  },
 }
 
 // 调用
 heo.changeMusicBg(false);
+
+// 改进vh
+const vh = window.innerHeight * 1;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 1;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+//获取url
+function extractValue(input) {
+  var valueRegex = /\("([^\s]+)"\)/g;
+  var match = valueRegex.exec(input);
+  return match[1];
+}
+
+//空格控制音乐
+document.addEventListener("keydown", function(event) {
+  if (event.code === "Space") {
+    event.preventDefault();
+    document.querySelector('meting-js').aplayer.toggle();
+  }
+});
